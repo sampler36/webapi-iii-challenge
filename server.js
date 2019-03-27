9ad23b50c002ade47b2478ec5b4b4682a97e0f0e
Talent Antonio <antonio.talenty@gmail.com>
2019-03-27 19:00:32 +0000

    Global middleware set up

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const server = express();
+
+server.use(helmet());
+server.use(morgan('dev'));
+
 
 server.get('/', (req, res) => {
   res.send('Hello World')
