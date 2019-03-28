e538872c406e17fe02765eab41c8c33365eed01d
Talent Antonio <antonio.talenty@gmail.com>
2019-03-28 10:30:44 +0000

    getById

--- server.js
+++ server.js
@@ -3,14 +3,32 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const usersRouter = require('./users/userRouter');
+
 const server = express();
 
 server.use(helmet());
 server.use(morgan('dev'));
+server.use(express.json());
 
+server.use('/users', usersRouter); // custom middleware
 
 server.get('/', (req, res) => {
   res.send('Hello World')
 });
+
+// function only() {
+//     return function(req, res, next) {
+//       const personName = (req.method === "POST" || req.method === "PUT");
+//       if(personName.toUpperCase() === name.toUpperCase()) {
+//         next();
+//       } else {
+//         res.status(401).json({
+//           message: 'You are not allowed mate'
+//         })
+//       }
+//     }
+//   }
+  
 
 module.exports = server;
---
+++ users/userRouter.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const Users = require("./userDb.js");
+
+const router = express.Router();
+
+router.use(express.json());
+
+router.get("/", (req, res) => {
+  Users.get()
+    .then((data) => res.json(data))
+    .catch((data) =>
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      })
+    );
+});
+router.get("/:id", (req, res) => {
+    Users.getById(req.params.id)
+      .then((data) => res.json(data))
+      .catch((data) =>
+        res.status(500).json({
+          errorMessage: "The users information could not be retrieved."
+        })
+      );
+  });
+
+module.exports = router;
